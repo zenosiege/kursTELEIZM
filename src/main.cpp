80b89d1828d5719e84474792b23285f719dd986f
Pantileev Danila <neeta7love@gmail.com>
2025-09-16 18:04:50 +0300

    Change setup to STM32F303x

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@ int main() {
     while (true) {
 
 
-    rcc_periph_clock_enable(RCC_GPIOD);
+    rcc_periph_clock_enable(RCC_GPIOE);
 
-    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
 
 
     while (true) {
 
 
-        gpio_toggle(GPIOD, GPIO15);
+        gpio_toggle(GPIOE, GPIO15);
 
 
         for (volatile uint32_t i = 0; i<2'000'000; ++i);
